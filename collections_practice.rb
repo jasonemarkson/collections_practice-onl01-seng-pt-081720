f9f75e5840041b4db912731aad0f226b229cab8d
Jason Markson <jasonmarkson76@gmail.com>
2020-08-28 03:59:18 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,43 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort{|x,y| y <=> x }
+end
+
+def sort_array_char_count(array)
+  array.sort{|x,y| x.length <=> y.length }
+end
+
+def swap_elements(array)
+  array[1,2] = array[2], array[1]
+  array 
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  i = 0
+  ke$ha_array = []
+  array.each do 
+  ke$ha_array << yield(array.string[3] = "$")
+  end
+  ke$ha_array
+end
+
+def find_a(array)
+  array.select do |word|
+    word.start_with?("a")
+  end
+end
+
+def sum_array(array)
+  array.inject{|x,y|x + y}
+end
+
+def add_s(array)
+  array.each_with_index.collect{|w, index| "#{w}s"}
+end
